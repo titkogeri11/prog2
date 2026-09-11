6232bb7c506bad518d8293999de725ab7c46c0e5
titkogergo <titkogergo@protonmail.com>
2026-09-11 15:33:53 +0200

    pentek 14:00, elso ora

---
+++ Pentek1.java
@@ -0,0 +1,26 @@
+public class Pentek1 {
+    
+
+    public static void main(String[] args) {
+        //System.out.println("Hello World!");
+
+        //boolean ok;
+        //ok = true;
+        //System.out.println(ok);
+
+
+        long big = 123456789012L;
+        System.out.println(big);
+
+        float vmi = 6.78f; //float vmi = (float)6.78;
+        System.out.println(vmi);
+
+        String text = "Hello";
+        System.out.println(text);
+
+        int[] tomb1 = new  int[3];
+        
+        
+        
+    }
+}
