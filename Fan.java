9a7cdba32886cd4dbd08d224f8e078ce82da8aa4
titkogergo <titkogergo@protonmail.com>
2026-09-24 00:04:39 +0200

    Fan.java, Account.java felig, teszter OK

--- Fan.java
+++ Fan.java
@@ -25,5 +25,49 @@ public class Fan{
         return (c1.equals(c2) && r1 == r2);
     }
 
+    public int getSpeed(){
+        return speed;
+    }
+
+    public void setSpeed(int speed){
+        this.speed = speed;
+    }
+
+    public boolean isOn(){
+        return on;
+    }
+
+    public void setOn(boolean on){
+        this.on = on;
+    }
+
+    public double getRadius(){
+        return radius;
+    }
+
+    public void setRadius(double radius){
+        this.radius = radius;
+    }
+
+    public String getColor(){
+        return color;
+    }
+
+    public void setColor(String color){
+        this.color = color;
+    }
+
+    @Override
+    public String toString(){
+        String result;
+        if (on){
+            result = "Fan speed=" + speed + ", color=" + color + ", radius=" + radius;
+        } else {
+            result = "fan is off, color=" + color + ", radius=" + radius;
+        }
+        System.out.println(result);
+        return result;
+    }
+
 
 }
--- Test_home.java
+++ Test_home.java
@@ -5,6 +5,21 @@ public class Test_home {
         Fan f1 = new Fan();
         Fan f2 = new Fan();
 
+        f1.setSpeed(3);
+        f1.setRadius(10);
+        f1.setColor("yellow");
+        f1.setOn(true);
+
+        f2.setSpeed(2);
+        f2.setRadius(5);
+        f2.setColor("blue");
+        f2.setOn(false);
+
+        f1.toString();
+        f2.toString();
+
+    
+
         
         
     }
